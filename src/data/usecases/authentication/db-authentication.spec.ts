357f0c6b779c192562ec74793e4a9a446a145e0e
Tanausu Sosa <tanausu_sa@hotmail.com>
2021-04-06 19:10:24 +0400

    test: ensure DbAuthentication returns null if LoadAccountByEmailRepository returns null

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -44,7 +44,6 @@ describe('DB Authentication Usecase', () => {
     const { sut, loadAccountByEmailRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
     await sut.auth(makeFakeAuthentication())
-
     expect(loadSpy).toHaveBeenCalledWith('any_email@email.com')
   })
 
@@ -52,7 +51,13 @@ describe('DB Authentication Usecase', () => {
     const { sut, loadAccountByEmailRepositoryStub } = makeSut()
     jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const promise = sut.auth(makeFakeAuthentication())
-
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Shoul return null if  LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBe(null)
   })
 })
