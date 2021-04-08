2ffb2da33707ac2ae100b0f6efca6a018bd9b94f
Tanausu Sosa <tanausu_sa@hotmail.com>
2021-04-08 19:38:06 +0400

    test: ensure AddSurveyController calls Validation with correct values

---
+++ src/presentacion/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -0,0 +1,48 @@
+import { AddSurveyController } from './add-survey-controller'
+import { HttpRequest, Validation } from './add-survey-controller-protocols'
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    question: 'any_question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer'
+      }
+    ]
+  }
+})
+
+const makeValidationStub = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+
+  return new ValidationStub()
+}
+
+interface SutTypes {
+  sut: AddSurveyController
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidationStub()
+  const sut = new AddSurveyController(validationStub)
+  return {
+    sut,
+    validationStub
+  }
+}
+
+describe('AddSurvey Controller', () => {
+  test('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    const htttpRequest = makeFakeRequest()
+    await sut.handle(htttpRequest)
+    expect(validateSpy).toHaveBeenCalledWith(htttpRequest.body)
+  })
+})
