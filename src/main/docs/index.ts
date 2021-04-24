7c31285df41b08c786d21bf224edab1cf0e4911b
Tanausu Sosa <tanausu_sa@hotmail.com>
2021-04-24 19:38:02 +0400

    feat: add swagger API

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'API from Mango\'s course to create surveys for coders.',
+    version: '1.0.0'
+  }
+}
