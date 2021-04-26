4e2b76609b15106affe06c6fda790b64298570f8
Tanausu Sosa <tanausu_sa@hotmail.com>
2021-04-26 21:05:07 +0400

    fix: addMockDate to controller test

--- src/main/factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory.ts
+++ src/main/factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory.ts
@@ -2,7 +2,7 @@ import { Controller } from '@/presentacion/protocols'
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
 import { makeDbLoadSurveyById } from '@/main/factories/usecases/survey-result/load-survey-by-id/db-load-survey-by-id-result-factory'
 import { makeDbLoadSurveyResult } from '@/main/factories/usecases/survey-result/load-survey-result/db-load-survey-result-factory'
-import { LoadSurveyResultController } from '@/presentacion/controllers/load-survey-result/load-survey-result-controller'
+import { LoadSurveyResultController } from '@/presentacion/controllers/survey-result/load-survey-result/load-survey-result-controller'
 
 export const makeLoadSurveyResultController = (): Controller => {
   const controller = new LoadSurveyResultController(makeDbLoadSurveyById(), makeDbLoadSurveyResult())
