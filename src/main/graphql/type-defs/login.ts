f26be47f8ceb21366d9dafeac9a196370c2a3e0f
Tanausu Sosa <tanausu_sa@hotmail.com>
2021-04-29 18:45:54 +0400

    feat: add signUp mutation

--- src/main/graphql/type-defs/login.ts
+++ src/main/graphql/type-defs/login.ts
@@ -6,6 +6,10 @@ export default gql`
     login (email: String!, password: String!): Account!
   }
 
+  extend type Mutation {
+    signUp (name: String!, email: String!, password: String!, passwordConfirmation: String!): Account!
+  }
+
   type Account {
     accessToken: String!
     name: String!
