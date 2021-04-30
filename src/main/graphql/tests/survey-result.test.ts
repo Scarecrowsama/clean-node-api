2ce84d5abdd908614c4c3a9758370a5c275f4319
Tanausu Sosa <tanausu_sa@hotmail.com>
2021-04-30 16:09:37 +0400

    test: ensure SurveyResult returns AccessDeniedError if no token is provided

--- src/main/graphql/tests/survey-result.test.ts
+++ src/main/graphql/tests/survey-result.test.ts
@@ -102,5 +102,30 @@ describe('SurveyResult GraphQL', () => {
         isCurrentAccountAnswer: false
       })
     })
+
+    test('Should return AccessDeniedError if no token is provided', async () => {
+      const surveyResult = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          {
+            image: 'http://image-name/image1.jpg',
+            answer: 'Answer 1'
+          },
+          {
+            image: 'http://image-name/image2.jpg',
+            answer: 'Answer 2'
+          }
+        ],
+        date: new Date()
+      })
+      const { query } = createTestClient({ apolloServer })
+      const res: any = await query(surveyResultQuery, {
+        variables: {
+          surveyId: surveyResult.ops[0]._id.toString()
+        }
+      })
+      expect(res.data).toBeFalsy()
+      expect(res.errors[0].message).toBe('Access Denied.')
+    })
   })
 })
